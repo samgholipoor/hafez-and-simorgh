ea01a5e32cc795fdfe70ce5719856d5b2daaae60
Sam Gholipour <sam.gholipour@sotoon.ir>
2025-01-25 14:58:34 +0330

    fix: minor issue

--- src/packages/Simorgh/src/constants/index.js
+++ src/packages/Simorgh/src/constants/index.js
@@ -1,4 +1,1 @@
-export const IS_PROD = process.env.NODE_ENV === 'production';
-export const API_BASE_URL = IS_PROD
-  ? '/api/'
-  : 'http://91.217.177.179:8112/api';
+export const API_BASE_URL = '/api/';
